b0e862f090ac6ea174d40db1f8c794ea3ae5a279
Atef Ben Ali <b.atef@devnet.me>
2019-06-16 11:50:22 +0100

    style: fix typo

--- src/books.js
+++ src/books.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    title: string
+    title: String
 });
 
 const Book = mongoose.model("book", BookSchema);
