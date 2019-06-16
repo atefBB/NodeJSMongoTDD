feef5ef297b2e80c3d65fa834061f56d04fbb122
Atef Ben Ali <b.atef@devnet.me>
2019-06-16 12:06:48 +0100

    review(test): drop `books` on each new `connection` test

--- test/connection_test.js
+++ test/connection_test.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/books_test", { 
-    useNewUrlParser: true 
+before((done) => {
+    mongoose.connect("mongodb://localhost/books_test", {
+        useNewUrlParser: true
+    });
+
+    mongoose.connection.once("open", () => {
+        console.log("Connection established");
+        done();
+    }).on("error", (err) => {
+        console.warn("Connection error: ", err);
+    });
 });
 
-mongoose.connection.once("open", () => {
-    console.log("Connection established")
-}).on("error", (err) => {
-    console.warn("Connection error: ", err);
+beforeEach("Delete old books", (done) => {
+    const {books} = mongoose.connection.collections;
+    books.drop(() => { 
+        done(); 
+    });
 });
